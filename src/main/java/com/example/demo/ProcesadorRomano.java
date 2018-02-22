c681fd3d74f6936f733b4a5202ed13483a78fe4e
Luis <luisgrases@gmail.com>
2018-02-21 19:59:04 -0600

    Implement: respeta la descendencia de valores

--- src/main/java/com/example/demo/ProcesadorRomano.java
+++ src/main/java/com/example/demo/ProcesadorRomano.java
@@ -2,30 +2,36 @@ package com.example.demo;
 
 public class ProcesadorRomano {
 	public static int convierte(String numerosRomanos) {
-		int result;
-		switch (numerosRomanos) {
-			case "":  result = 0;
-	         	break;
-	        case "I":  result = 1;
-	                 break;
-	        case "II":  result = 2;
-	                 break;
-	        case "III":  result = 3;
-	                 break;
-	        case "V":  result = 5;
-	                 break;
-	        case "X":  result = 10;
-	                 break;
-	        case "L":  result = 50;
-	                 break;
-	        case "C":  result = 100;
-	                 break;
-	        case "D":  result = 500;
-	                 break;
-	        case "M": result = 1000;
-	                 break;
-	        default: throw new IllegalArgumentException();
+		int result = 0;
+		int ICounter = 0;
+		int XCounter = 0;
+		int LCounter = 0;
+		int CCounter = 0;
+		int DCounter = 0;
+		
+		int lastAdded = Integer.MAX_VALUE;;
+		
+		for (char ch: numerosRomanos.toCharArray()) {
+			switch (ch) {
+		        case 'I':  if(ICounter < 3 && lastAdded >= 1) { result += 1; ICounter++; lastAdded = 1;} else throw new IllegalArgumentException();
+		                 break;
+		        case 'V':  if (lastAdded >= 5) { result += 5; lastAdded = 5;} else throw new IllegalArgumentException();
+		                 break;
+		        case 'X':  if(XCounter < 3 && lastAdded >= 10) { result += 10; XCounter++; lastAdded = 10;} else throw new IllegalArgumentException();
+		                 break;
+		        case 'L':  if(LCounter < 1 && lastAdded >= 50) {result += 50; LCounter++; lastAdded = 50;} else throw new IllegalArgumentException();
+		                 break;
+		        case 'C':  if(CCounter < 3 && lastAdded >= 100) {result += 100; CCounter++; lastAdded = 100;} else throw new IllegalArgumentException();
+		                 break;
+		        case 'D':  if(DCounter < 1 && lastAdded >= 500) {result += 500; DCounter++; lastAdded = 500;} else throw new IllegalArgumentException();
+		                 break;
+		        case 'M': if(lastAdded >= 1000) { result += 1000; lastAdded = 1000; } else throw new IllegalArgumentException();
+		                 break;
+		        default: throw new IllegalArgumentException();
+			}
 		}
 		return result;
 	}
+		
+		
 }
