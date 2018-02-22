1bb5fea2f84eac69f095f7b0d229d3e085152a30
Luis <luisgrases@gmail.com>
2018-02-21 19:35:16 -0600

    Refactor: No puede recibir mas de la cantidad especificada para cada caracter

--- src/test/java/com/example/demo/ProcesadorRomanoTest.java
+++ src/test/java/com/example/demo/ProcesadorRomanoTest.java
@@ -5,27 +5,59 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ProcesadorRomanoTest {
-
+	
+	
+	// convierte
+	
 	@Test
-	public void convierteRetorna0SiRecibeStringVacio() {
+	public void retorna0SiRecibeStringVacio() {
 		assertEquals(ProcesadorRomano.convierte(""), 0);
 	}
 	
 	@Test
-	public void convierteRetorna1SiRecibeUnaIMayuscula() {
+	public void retorna1SiRecibeUnaIMayuscula() {
 		assertEquals(ProcesadorRomano.convierte("I"), 1);
 	}
 	
 	@Test
-	public void convierteRetorna2SiRecibeDosIMayuscula() {
+	public void retorna2SiRecibeDosIMayuscula() {
 		assertEquals(ProcesadorRomano.convierte("II"), 2);
 	}
 	
 	@Test
-	public void convierteRetorna3SiRecibeTresIMayuscula() {
+	public void retorna3SiRecibeTresIMayuscula() {
 		assertEquals(ProcesadorRomano.convierte("III"), 3);
 	}
 	
+	@Test
+	public void retorna5SiRecibeUnaV() {
+		assertEquals(ProcesadorRomano.convierte("V"), 5);
+	}
+	
+	@Test
+	public void retorna10SiRecibeUnaX() {
+		assertEquals(ProcesadorRomano.convierte("X"), 10);
+	}
+	
+	@Test
+	public void retorna50SiRecibeUnaL() {
+		assertEquals(ProcesadorRomano.convierte("L"), 50);
+	}
+	
+	@Test
+	public void retorna100SiRecibeUnaC() {
+		assertEquals(ProcesadorRomano.convierte("C"), 100);
+	}
+	
+	@Test
+	public void retorna500SiRecibeUnaD() {
+		assertEquals(ProcesadorRomano.convierte("D"), 500);
+	}
+	
+	@Test
+	public void retorna1000SiRecibeUnaM() {
+		assertEquals(ProcesadorRomano.convierte("M"), 1000);
+	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void convierteRetornaErrorSiRecibeCuatroIMayuscula() {
@@ -77,37 +109,6 @@ public class ProcesadorRomanoTest {
 	public void convierteRetornaErrorSiRecibe3DIMayuscula() {
 		ProcesadorRomano.convierte("DDD");
 	}
-	
-	@Test
-	public void convierteRetorna5SiRecibeUnaV() {
-		assertEquals(ProcesadorRomano.convierte("V"), 5);
-	}
-	
-	@Test
-	public void convierteRetorna10SiRecibeUnaX() {
-		assertEquals(ProcesadorRomano.convierte("X"), 10);
-	}
-	
-	@Test
-	public void convierteRetorna50SiRecibeUnaL() {
-		assertEquals(ProcesadorRomano.convierte("L"), 50);
-	}
-	
-	@Test
-	public void convierteRetorna100SiRecibeUnaC() {
-		assertEquals(ProcesadorRomano.convierte("C"), 100);
-	}
-	
-	@Test
-	public void convierteRetorna500SiRecibeUnaD() {
-		assertEquals(ProcesadorRomano.convierte("D"), 500);
-	}
-	
-	@Test
-	public void convierteRetorna1000SiRecibeUnaM() {
-		assertEquals(ProcesadorRomano.convierte("M"), 1000);
-	}
-	
 	
 	
 
