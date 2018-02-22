c40344c785b777c773a8985a317ffc18ba0dbaa7
Luis <luisgrases@gmail.com>
2018-02-21 18:51:18 -0600

    First Commit

---
+++ src/main/java/com/example/demo/Kata1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata1Application.class, args);
+	}
+}
