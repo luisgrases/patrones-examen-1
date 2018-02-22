4d966d5f9e7cdd791dd9b80a79d0e4e77356833c
Luis <luisgrases@gmail.com>
2018-02-21 19:25:20 -0600

    Implement: letras corresponden con sus numeros. Error si cuatro IIII son suplidos a la función

--- src/main/java/com/example/demo/ProcesadorRomano.java
+++ src/main/java/com/example/demo/ProcesadorRomano.java
@@ -2,6 +2,31 @@ package com.example.demo;
 
 public class ProcesadorRomano {
 	public static int convierte(String string) {
-		return 0;
+		int result;
+		switch (string) {
+			case "":  result = 0;
+	         	break;
+	        case "I":  result = 1;
+	                 break;
+	        case "II":  result = 2;
+	                 break;
+	        case "III":  result = 3;
+	                 break;
+	        case "IIII":  throw new IllegalArgumentException();
+	        case "V":  result = 5;
+	                 break;
+	        case "X":  result = 10;
+	                 break;
+	        case "L":  result = 50;
+	                 break;
+	        case "C":  result = 100;
+	                 break;
+	        case "D":  result = 500;
+	                 break;
+	        case "M": result = 1000;
+	                 break;
+	        default: throw new IllegalArgumentException();
+		}
+		return result;
 	}
 }
